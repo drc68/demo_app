c64c60371c022728db74fe9b573b8df331688a31
drc68 <doug.colligan@gmail.com>
2013-07-04 15:12:16 -0400

    Done with demo app

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130704184049) do
+ActiveRecord::Schema.define(version: 20130704185545) do
+
+  create_table "mircoposts", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
